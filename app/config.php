404aaf80d1245d8222651a236052e1cfd346c20b
Epic_F <fadiabdalkhalk@gmail.com>
2026-03-11 16:15:59 +0100

    Remove email address from config.php header
    Removed email address from the config file header.

--- app/config.php
+++ app/config.php
@@ -4,13 +4,12 @@
  *            .---._------------------------------+
  *___________/ ._____)                            |
  *              ) __|         created by          |
- *                __|      Fadi Abdalkhalk        |
+ *                __|          Epic_F             |
  *..---------.._____|                             |
  *                  +-----------------------------+
  *
  *	copyright 2014 - Gitcloud
  *	created on 13-06-2014
- *	email: fadiabdalkhalk@gmail.com
  * 	based on https://github.com/IcyApril/TPCrawler
  * 
  */
